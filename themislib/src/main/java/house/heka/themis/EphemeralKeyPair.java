056a733f9c0a190e36c8ed7beb840fa831ad38b6
Aron Price <aron23@gmail.com>
2016-08-09 12:48:13 -0400

    eliminated ephemeral keys being written to file

--- themislib/src/main/java/house/heka/themis/EphemeralKeyPair.java
+++ themislib/src/main/java/house/heka/themis/EphemeralKeyPair.java
@@ -6,53 +6,24 @@ import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.UUID;
 
 import javax.crypto.spec.SecretKeySpec;
 
-/**
- * Stores a keypair and associated UUID
- */
+
 public class EphemeralKeyPair {
-    private String eid;
-    private KeyPair ekeys;
+    private final PublicKey pubKey;
+    private final PrivateKey privKey;
 
     public EphemeralKeyPair(KeyPair kp, Context context) {
-        ekeys = kp;
-        eid = UUID.randomUUID().toString();
-        try {
-            LocalPref.putStringPref(context, "ephemeral_public:" + eid, Encrypt.publicKey2Str(kp.getPublic()));
-            LocalPref.putStringPref(context, "ephemeral_private:" + eid, Encrypt.privateKey2Str(kp.getPrivate()));
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static PublicKey getPublic(Context context, String eid) throws GeneralSecurityException {
-        String pub = LocalPref.getStringPref(context, "ephemeral_public:" + eid);
-        if (pub != null)
-            return Encrypt.str2publicKey(pub);
-        return null;
-    }
-
-    public static SecretKeySpec generateSharedSecret(Context context, String eid, PublicKey publicKey) throws GeneralSecurityException {
-        String priv_str = LocalPref.getStringPref(context, "ephemeral_private:" + eid);
-        if (priv_str != null) {
-            PrivateKey priv = Encrypt.str2privateKey(priv_str);
-            return Encrypt.generateSharedSecret(priv, publicKey);
-        }
-        return null;
-    }
-
-    public String getEid() {
-        return eid;
+        pubKey = kp.getPublic();
+        privKey = kp.getPrivate();
     }
 
     public PublicKey getPublic() {
-        return ekeys.getPublic();
+        return pubKey;
     }
 
     public SecretKeySpec generateSharedSecret(PublicKey publicKey) throws GeneralSecurityException {
-        return Encrypt.generateSharedSecret(ekeys.getPrivate(), publicKey);
+        return Encrypt.generateSharedSecret(privKey, publicKey);
     }
 }
